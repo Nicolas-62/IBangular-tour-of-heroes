a5651ff245a015a292bff19348ad6ac1aae071fa
Lourdel <nlourdel62000@gmail.com>
2019-11-27 17:00:40 +0100

    suite cours HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -13,19 +13,23 @@ export class HeroDetailComponent implements OnInit {
 
   @Input()
   hero: Hero;
-  
+
   constructor(private route: ActivatedRoute,
               private heroService: HeroService,
               private location: Location) { }
 
   ngOnInit() {
+    this.getHero();
   }
   getHero(): void{
     const id = +this.route.snapshot.paramMap.get('id'); // convertir chaine en nombre, ajouter +
-    this.heroService.getHero(id);
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
   goBack(): void {
     this.location.back();
+  }
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 
 }
